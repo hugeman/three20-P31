449ef48c1e861dca1d42880dbcacdbf40ff62165
uprise78 <desaro@gmail.com>
2010-05-08 19:52:36 -0700

    Added a proper static library project.  Modular just like the new Three20

---
+++ P31-Additions/P31Categories/TTViewControllerCategory.m
@@ -0,0 +1,36 @@
+//
+//  TTViewControllerCategory.m
+//  P31-Additions
+//
+//  Created by Mike on 5/8/10.
+//  Copyright 2010 Prime31 Studios. All rights reserved.
+//
+
+#import "TTViewControllerCategory.h"
+
+
+@implementation TTViewController(Category)
+
+- (void)setSearchViewController:(TTTableViewController*)searchViewController {
+	if (searchViewController) {
+		if (nil == _searchController) {
+			UISearchBar* searchBar = [[[UISearchBar alloc] init] autorelease];
+			[searchBar sizeToFit];
+
+			searchBar.autoresizingMask = UIViewAutoresizingFlexibleWidth; // P31 Addition
+			searchBar.tintColor = TTSTYLEVAR(searchBarTintColor); // P31 Addition
+			
+			_searchController = [[TTSearchDisplayController alloc] initWithSearchBar:searchBar
+																  contentsController:self];
+		}
+		
+		searchViewController.superController = self;
+		_searchController.searchResultsViewController = searchViewController;
+		
+	} else {
+		_searchController.searchResultsViewController = nil;
+		TT_RELEASE_SAFELY(_searchController);
+	}
+}
+
+@end
